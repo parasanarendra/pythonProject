7555319588bb074a2d0c6100c462c1a8ff8c48e9
projects <parasanarendra3@gmail.com>
2022-10-21 14:02:35 +0530

    Initial Commit

---
+++ star.py
@@ -0,0 +1,21 @@
+totalSeconds = 400
+
+secondsPerMinute = 60
+secondsPerHour = 60 * secondsPerMinute
+secondsPerDay = 24 * secondsPerHour
+
+days = totalSeconds // secondsPerDay
+hours = (totalSeconds - days * secondsPerDay) // secondsPerHour
+minutes = (totalSeconds - days * secondsPerDay - hours * secondsPerHour) // secondsPerMinute
+seconds = totalSeconds - days * secondsPerDay - hours * secondsPerHour - minutes * secondsPerMinute
+
+if days > 0:
+    print(days, 'Days', end=' ')
+if hours > 0:
+    print(hours, 'Hours', end=' ')
+if minutes > 0:
+    print(minutes, 'Minutes', end=' ')
+if seconds > 0:
+    print(seconds, 'Seconds', end=' ')
+print()
+print("popup display")
